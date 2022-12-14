d6e210eaff5fc11e66b504368b21762111799e12
apranko <andrei.pranko@solbeg.com>
2022-12-14 18:51:39 +0300

    init

---
+++ app/src/main/java/com/example/tensorflowrecognition/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.tensorflowrecognition
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tensorflowrecognition.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
